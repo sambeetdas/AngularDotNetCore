eeade7a27cb1db8cab8194cf88e87fdfb3c41b1d
Sambeet Das <sambeet.gugul@gmail.com>
2021-03-18 18:33:57 +0530

    Angular app folder changes

--- DM/DM.UI/Startup.cs
+++ DM/DM.UI/Startup.cs
@@ -20,7 +20,6 @@ namespace DM.UI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
@@ -36,7 +35,6 @@ namespace DM.UI
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -58,6 +56,9 @@ namespace DM.UI
 
             app.UseSpa(spa =>
             {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
